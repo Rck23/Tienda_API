7d4a8fc489c38b5a814917745a0482102f73d9cf
Ulises Martinez <ulimxtz@gmail.com>
2023-03-25 17:22:51 -0600

    Agregar archivos de proyecto.

---
+++ API/Controllers/ProductosController.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Core.Entities;
+using Grpc.Core;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductosController : BaseApiController
+{
+    private readonly TiendaContext _context;
+    public ProductosController(TiendaContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Producto>>> Get()
+    {
+        var productos = await _context.Productos.ToListAsync();
+
+        return Ok(productos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> Get(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+
+        return Ok(producto);
+    }
+}
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        //PERMITIMOS ACCESO DESDE CUALCUQUIER ORIGEN, ACCESO Y ENCABEZADO
+        //SOLO APLICA PARA DESARROLLO (EN PRODUCCION HAY QUE ESPECIFICAR TODO!)
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin() // Dominios (http://localhost:3000, http://Pagina.com)
+                .AllowAnyMethod() // metodos como GET, POST
+                .AllowAnyHeader()); // Aceptacion de "Content-Type", "Accept"
+        });
+}
